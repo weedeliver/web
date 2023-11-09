be10216d47e67242148f57939fa00979305c6f19
Erlantz <96289199+erlgr@users.noreply.github.com>
2023-11-09 14:30:39 +0100

    aldaketa minimoak css produktua

--- weedeliver/web/static/js/produktua.js
+++ weedeliver/web/static/js/produktua.js
@@ -1,13 +1,19 @@
 function increment() {
+  let number = parseInt(document.getElementById('kantitatea').value, 10);
   let numberInput = document.getElementById('kantitatea');
-  numberInput.value = parseInt(numberInput.value, 10) + 1;
-  if(numberInput.value > 100) numberInput.value = 100; // to enforce the max value
+  let numberVisible = document.getElementById('kantitateav');
+  number++;
+  if(number > 99) number = 99; // to enforce the max value
+  numberInput.value = number;
+  numberVisible.innerText = number.toString();
 }
 
 function decrement() {
+  let number = parseInt(document.getElementById('kantitatea').value, 10);
   let numberInput = document.getElementById('kantitatea');
-  let currentValue = parseInt(numberInput.value, 10);
-  if (currentValue > 1) { // changed from 0 to 1 to respect the min value
-    numberInput.value = currentValue - 1;
-  }
+  let numberVisible = document.getElementById('kantitateav');
+  number--;
+  if(number < 1) number = 1; // to enforce the max value
+  numberInput.value = number;
+  numberVisible.innerText = number.toString();
 }
